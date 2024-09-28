64e71855eb792349a5a62bb8c198a58f713d251a
Jonathan Adiwibowo <jonathanadi46@gmail.com>
2024-09-28 18:51:24 +0700

    feat: separate to implementation

--- internal/book/service/book_service.go
+++ internal/book/service/book_service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"bookstore/internal/book/model"
-	"bookstore/internal/book/repository"
 )
 
 type BookService interface {
@@ -10,32 +9,4 @@ type BookService interface {
 	GetBooks() ([]model.Book, error)
 	GetBookById(id int) (model.Book, error)
 	UpdateBook(book *model.Book) error
-}
-
-type service struct {
-	repository repository.BookRepository
-}
-
-func NewService(repository repository.BookRepository) BookService {
-	return &service{repository: repository}
-}
-
-// CreateBook implements Service.
-func (s *service) CreateBook(book *model.Book) (int, error) {
-	return s.repository.CreateBook(book)
-}
-
-// GetBookById implements Service.
-func (s *service) GetBookById(id int) (model.Book, error) {
-	return s.repository.GetBookById(id)
-}
-
-// GetBooks implements Service.
-func (s *service) GetBooks() ([]model.Book, error) {
-	return s.repository.GetBooks()
-}
-
-// UpdateBook implements Service.
-func (s *service) UpdateBook(book *model.Book) error {
-	return s.repository.UpdateBook(book)
 }
---
+++ internal/book/service/book_service_impl.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"bookstore/internal/book/model"
+	"bookstore/internal/book/repository"
+)
+
+type service struct {
+	repository repository.BookRepository
+}
+
+func NewService(repository repository.BookRepository) BookService {
+	return &service{repository: repository}
+}
+
+// CreateBook implements Service.
+func (s *service) CreateBook(book *model.Book) (int, error) {
+	return s.repository.CreateBook(book)
+}
+
+// GetBookById implements Service.
+func (s *service) GetBookById(id int) (model.Book, error) {
+	return s.repository.GetBookById(id)
+}
+
+// GetBooks implements Service.
+func (s *service) GetBooks() ([]model.Book, error) {
+	return s.repository.GetBooks()
+}
+
+// UpdateBook implements Service.
+func (s *service) UpdateBook(book *model.Book) error {
+	return s.repository.UpdateBook(book)
+}
